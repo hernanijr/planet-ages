5226ae6d307f3f292b214c26cf5512fbc46372ad
Hernani Junior <hernani.junior13@gmail.com>
2023-12-12 20:52:26 -0300

    create calculate planet age

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('calculate-age')
+  calculateAge(@Query('seconds') seconds: number, @Query('type') type: string) {
+    return this.appService.calculateAge(seconds, type);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,34 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  private planets = [
+    { name: 'Mercury', orbitalPeriod: 0.2408467 },
+    { name: 'Venus', orbitalPeriod: 0.61519726 },
+    { name: 'Earth', orbitalPeriod: 1.0 },
+    { name: 'Mars', orbitalPeriod: 1.8808158 },
+    { name: 'Jupiter', orbitalPeriod: 11.862615 },
+    { name: 'Saturn', orbitalPeriod: 29.447498 },
+    { name: 'Uranus', orbitalPeriod: 84.016846 },
+    { name: 'Neptune', orbitalPeriod: 164.79132 },
+  ];
+
+  calculateAge(seconds: number, type: string): string {
+    if (
+      seconds <= 0 ||
+      !this.planets.some(
+        (planet) => planet.name.toLowerCase() === type.toLowerCase(),
+      )
+    ) {
+      throw new Error('Invalid input');
+    }
+
+    const earthYears = seconds / 60 / 60 / 24 / 365.25;
+    const planet = this.planets.find(
+      (p) => p.name.toLowerCase() === type.toLowerCase(),
+    );
+
+    const planetYears = earthYears * planet.orbitalPeriod;
+
+    return `${planetYears.toFixed(2)} ${planet.name}-years-old`;
   }
 }
